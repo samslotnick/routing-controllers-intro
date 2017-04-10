929119e5aab0b80b18631618a86ddbde90751556
Sam Slotnick <samslotnick@Sams-MacBook-Pro.local>
2017-04-10 13:53:05 -0400

    instance var

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,12 @@
 class PagesController < ApplicationController
+    def welcome
+      @header = "Chooo choooo ruby on rails"
+    end
+
+    def about
+      render :about
+    end
+    def contest
+      render :contest
+    end
 end
