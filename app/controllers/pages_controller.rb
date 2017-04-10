9dac8117013582bbc92825e55d1c385ab8aa984d
Sam Slotnick <samslotnick@Sams-MacBook-Pro.local>
2017-04-10 14:13:57 -0400

    refactoring

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,9 +9,23 @@ class PagesController < ApplicationController
     def contest
       render :contest
     end
+    before_action :set_kitten_url, only: [:kitten, :kittens]
     def kitten
-      requested_size = params[:size]
-      @kitten_url = "http://lorempixel.com/#{requested_size}/#{requested_size}/cats"
-    #  render :kitten
     end
+    def kittens
+    end
+    # def kitten
+    #   requested_size = params[:size]
+    #   @kitten_url = "http://lorempixel.com/#{requested_size}/#{requested_size}/cats"
+    # #  render :kitten
+    # end
+    # def kitten
+    #   requested_size = params[:size]
+    #   @kitten_url = "http://lorempixel.com/#{requested_size}/#{requested_size}/cats"
+    #   end
+
+  def set_kitten_url
+  requested_size = params[:size]
+  @kitten_url = "http://lorempixel.com/#{requested_size}/#{requested_size}/cats"
+  end
 end
